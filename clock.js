7ccbe003aea929a9061ff2cd7bcd3f9c9f3ec64e
kswiton <64925239+kswiton@users.noreply.github.com>
2021-12-15 17:26:43 +0100

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,6 @@
+function calculateAngle(hours, minutes) {
+    let minutesAngle = minutes * (360 / 60);
+    let hoursAngle = (hours % 12) * 30 + (minutes/2);
+    result = Math.abs(hoursAngle - minutesAngle);
+    return Math.min(result, 360 - result);
+    }
